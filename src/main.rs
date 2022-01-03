a72231f7189362d5a63ff3bbd8e765714efe087f
David Itkin <ditkin@eucleo.com>
2022-01-02 22:17:03 -0500

    Cleanup and implement some addressing modes.  Creating of instruction and addressing mode variants made a little cleaner.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use rust_6502_emulator::processor::{create6502, ProcessorTrait};
 fn main() {
     let bus: Rc<RefCell<dyn Bus>> = Rc::new(RefCell::new(SimpleBus { registered: vec![] }));
 
-    let processor = Rc::new(RefCell::new(create6502(Rc::clone(&bus))));
+    let processor = Rc::new(RefCell::new(create6502()));
     let memory: Rc<RefCell<Memory>> = Rc::new(RefCell::new(Memory::new(0x0f00, 0xffff)));
 
     memory
         .borrow_mut()
-        .write(0xffc, vec![0xfe, 0x0f, 0xea, 0x4c, 0xfe, 0x0f]);
+        .write(0xffc, vec![0xfe, 0x0f, 0xea, 0x4c, 0xfe, 0x0f, 0xfe, 0x0f]);
     bus.borrow_mut()
         .register_device(&memory.borrow_mut().as_cloned_bus_device(Rc::clone(&memory)));
     //rc_bus.borrow_mut().register_device(&processor.borrow_mut().as_cloned_bus_device(Rc::clone(&processor)));
--- src/processor.rs
+++ src/processor.rs
@@ -31,6 +31,9 @@ pub enum AddressRegister {
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub enum InternalOperations {
     NOP,
+    ReadAddressLo,
+    ReadAddressHi,
+    IncrementPCBySignedOperand,
     IncrementAddressByReg {
         reg: DataRegister,
     },
@@ -89,11 +92,10 @@ pub enum AddressingMode {
 }
 
 // Implementation of an instruction. addressing mode specific
-struct Instruction {
+pub struct Instruction {
     pub mnemonic: String,
     pub operations: Vec<InternalOperations>,
-    pub addressing: AddressingMode,
-    pub can_overlap_with_next_fetch: bool,
+    pub addressing: AddressingMode
 }
 
 pub struct Proc6502 {
@@ -108,81 +110,77 @@ pub struct Proc6502 {
     instructions: HashMap<u8, Instruction>,
 }
 
-pub fn fetch_operations_for_mode(mode: AddressingMode) -> Vec<InternalOperations> {
+pub fn fetch_operations_for_mode(mode: &AddressingMode) -> Vec<InternalOperations> {
     match mode {
         Accumulator => vec![],
         Absolute => vec![FetchAddrLo, FetchAddrHi],
-        AbsIndexed { reg } => vec![FetchAddrLo, FetchAddrHi, IncrementAddressByReg { reg }],
+        AbsIndexed { reg } => vec![FetchAddrLo, FetchAddrHi, IncrementAddressByReg { reg: *reg }],
         Immediate => vec![FetchImmediateOperand],
         Implied => vec![],
-        Indirect => vec![FetchAddrLo, FetchAddrHi, FetchOperand],
-        IndexedIndirect => todo!(),
-        IndirectIndexed => todo!(),
-        Relative => todo!(),
+        Indirect => vec![FetchAddrLo, FetchAddrHi, ReadAddressLo, ReadAddressHi],
+        IndexedIndirect => vec![], // TODO
+        IndirectIndexed => vec![], // TODO
+        Relative => vec![FetchOperand, IncrementPCBySignedOperand],
         ZeroPage => vec![FetchZeroPageAddr],
-        ZeroPageIndexed { reg } => todo!(),
+        ZeroPageIndexed { reg } => vec![FetchZeroPageAddr, IncrementAddressByReg {reg: *reg}, FetchOperand],
     }
 }
 
-pub fn concat_operations(
-    v1: Vec<InternalOperations>,
-    v2: Vec<InternalOperations>,
-) -> Vec<InternalOperations> {
-    let mut v: Vec<InternalOperations> = vec![];
-    v.extend(v1.iter().copied());
-    v.extend(v2.iter().copied());
-    v
+pub fn create_instructions(th_mnemonic: &str, modes: Vec<(u8, AddressingMode)>, the_operations: &[InternalOperations]) -> Vec<(u8, Instruction)> {
+    let mut instructions: Vec<(u8, Instruction)> = vec!();
+
+    for (opcode, mode) in modes.iter() {
+        let mut operations = fetch_operations_for_mode(mode);
+        operations.extend(the_operations);
+        let x = Instruction {
+            mnemonic: th_mnemonic.to_string(),
+            operations,
+            addressing: *mode,
+        };
+        instructions.push((*opcode, x));
+    }
+    instructions
 }
 
-pub fn create6502(bus: Rc<RefCell<dyn Bus>>) -> Proc6502 {
+pub fn create6502() -> Proc6502 {
     let mut map_o_instructions: HashMap<u8, Instruction> = HashMap::new();
-    map_o_instructions.insert(
-        0xea,
-        Instruction {
-            mnemonic: "NOP".to_string(),
-            operations: vec![NOP],
-            addressing: Implied,
-            can_overlap_with_next_fetch: false,
-        },
-    );
-    map_o_instructions.insert(
-        0xa9,
-        Instruction {
-            mnemonic: "LDA".to_string(),
-            addressing: Absolute,
-            operations: concat_operations(
-                fetch_operations_for_mode(Absolute),
-                vec![StoreToAccumulator {
-                    src: InternalOperand,
-                }],
-            ),
-            can_overlap_with_next_fetch: true,
-        },
-    );
-    map_o_instructions.insert(
-        0x8d,
-        Instruction {
-            mnemonic: "STA Oper".to_string(),
-            addressing: Absolute,
-            operations: concat_operations(
-                fetch_operations_for_mode(Absolute),
-                vec![WriteToAddress {
-                    src: A,
-                    addr: InternalAddress,
-                }],
-            ),
-            can_overlap_with_next_fetch: false,
-        },
-    );
-    map_o_instructions.insert(
-        0x4c,
-        Instruction {
-            mnemonic: "JMP $XXXX".to_string(),
-            operations: concat_operations(fetch_operations_for_mode(Absolute), vec![JumpToAddress]),
-            addressing: Absolute,
-            can_overlap_with_next_fetch: false,
-        },
-    );
+
+    let lda_modes = vec![
+        (0xa9, Immediate),
+        (0xa5, ZeroPage),
+        (0xb5, ZeroPageIndexed {reg: X}),
+        (0xad, Absolute),
+        (0xbd, AbsIndexed {reg: X}),
+        (0xb9, AbsIndexed {reg: X}),
+        (0xa1, IndexedIndirect),
+        (0xb1, IndirectIndexed)
+    ];
+    let lda_instructions = vec![FetchOperand, StoreToAccumulator {src: InternalOperand}];
+
+    let sta_modes = vec![
+        (0x85, ZeroPage),
+        (0x95, ZeroPageIndexed {reg: X}),
+        (0x8d, Absolute),
+        (0x8d, AbsIndexed {reg: X}),
+        (0x99, AbsIndexed {reg: X}),
+        (0x81, IndexedIndirect),
+        (0x91, IndirectIndexed)
+    ];
+    let sta_instructions = vec![WriteToAddress {src: A, addr: InternalAddress}];
+
+    let jmp_modes = vec![
+        (0x4c, Absolute),
+        (0x6c, Indirect)
+    ];
+    let jmp_instructions = vec![JumpToAddress];
+
+    let nop_modes = vec![(0xea, Implied)];
+    let nop_instructions = vec![NOP];
+
+    map_o_instructions.extend(create_instructions("JMP", jmp_modes, &jmp_instructions));
+    map_o_instructions.extend(create_instructions("LDA", lda_modes, &lda_instructions));
+    map_o_instructions.extend(create_instructions("STA", sta_modes, &sta_instructions));
+    map_o_instructions.extend(create_instructions("NOP", nop_modes, &nop_instructions));
 
     let mut p = Proc6502 {
         pc: 0x0FFC,
@@ -196,6 +194,7 @@ pub fn create6502(bus: Rc<RefCell<dyn Bus>>) -> Proc6502 {
         instructions: map_o_instructions,
     };
 
+    // Prime the operation_stream with the boot sequence
     p.pc = 0x0FFC; // BOOT location
     p.operation_stream.extend(
         vec![FetchAddrLo, FetchAddrHi, JumpToAddress]
@@ -227,11 +226,6 @@ impl Proc6502 {
         let rc: Rc<RefCell<dyn BusDevice>> = me;
         Rc::clone(&rc)
     }
-}
-
-
-fn read_instructions() -> HashMap<u8, Instruction> {
-    todo!()
 }
 
 impl ProcessorTrait for Proc6502 {
@@ -289,7 +283,14 @@ impl ProcessorTrait for Proc6502 {
             AddIndexLo => {}
             AluIncr => {}
             InternalOperations::IncrementAddressByReg { .. } => {}
-            FetchZeroPageAddr => {}
+            FetchZeroPageAddr => {
+                self.internal_address &= 0x0000;
+                self.internal_address = the_bus.borrow().read(self.pc) as Address;
+                self.pc += 1;
+            }
+            IncrementPCBySignedOperand => {}
+            ReadAddressLo => {}
+            ReadAddressHi => {}
         }
     }
 
